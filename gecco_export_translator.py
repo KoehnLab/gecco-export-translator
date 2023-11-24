dbd93431ae345b6434d8e435ab1d8005093d2fc0
Robert Adam <dev@robert-adam.de>
2023-11-24 09:32:11 +0100

    Implement proper whitespace skipping

--- gecco_export_translator.py
+++ gecco_export_translator.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
 
 import pe
+from pe.operators import Star, Class
 
 import os
 
 script_dir: str = os.path.dirname(os.path.realpath(__file__))
 
+
 def main():
-    grammar = pe.compile(open(os.path.join(script_dir, "gecco_export_grammar.peg"), "r").read())
-    grammar.match("[END]")
+    grammar_definition = open(
+        os.path.join(script_dir, "gecco_export_grammar.peg"), "r"
+    ).read()
+    grammar = pe.compile(grammar_definition, ignore=Star(Class(" \t\r\v\f")))
+    grammar.match(" [END]")
+
 
 if __name__ == "__main__":
     main()
