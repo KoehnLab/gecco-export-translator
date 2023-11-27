47fa0a59cfc10e4a08b35e5ba590acea8eda0647
Robert Adam <dev@robert-adam.de>
2023-11-27 16:20:15 +0100

    Continue work on AST structure elements

--- bin/gecco_export_translator.py
+++ bin/gecco_export_translator.py
@@ -8,18 +8,49 @@ import os
 import argparse
 from dataclasses import dataclass
 
+
 @dataclass
-class Tensor():
+class IndexGroup:
+    creators: List[str]
+    annihilators: List[str]
+
+
+@dataclass
+class Tensor:
     name: str
-    factor: float
+    indexing: List[IndexGroup]
+    transposed: bool
+
+
+@dataclass
+class Arc:
+    first_vertex_idx: int
+    second_vertex_idx: int
+    indices: IndexGroup
+
+
+@dataclass
+class ExternalArc:
+    origin_vertex_idx: int
+    result_super_vertex: int
+    indices: IndexGroup
+
+
+@dataclass
+class Index:
+    id: int
+    space: int
+    vertex: int
+    type: int
 
 
 @dataclass
 class Contraction:
     id: int
     result: Tensor
-    operators: List[Tensor]
     super_vertex_association: List[int]
+    vertices: List[Tensor]
+
 
 class MyTransformer(Transformer):
     def contraction(self, items):
@@ -32,7 +63,6 @@ class MyTransformer(Transformer):
         return int(value)
 
 
-
 def read_grammar() -> str:
     script_dir: str = os.path.dirname(os.path.realpath(__file__))
     grammar_dir = os.path.realpath(os.path.join(script_dir, "..", "grammar"))
@@ -43,6 +73,7 @@ def read_grammar() -> str:
 
     return grammar
 
+
 def get_parser(parser: str = "lalr") -> Lark:
     return Lark(read_grammar(), parser=parser)
 
@@ -51,8 +82,12 @@ def main():
     argument_parser = argparse.ArgumentParser(
         description="A script capable of parsing an export file generated via GeCCo and translating it to different formats"
     )
-    argument_parser.add_argument("export_file", help="Path to the GeCCo export file that shall be translated")
-    argument_parser.add_argument("--format", choices=["tex"], default="tex", help="The desired output format")
+    argument_parser.add_argument(
+        "export_file", help="Path to the GeCCo export file that shall be translated"
+    )
+    argument_parser.add_argument(
+        "--format", choices=["tex"], default="tex", help="The desired output format"
+    )
 
     args = argument_parser.parse_args()
 
