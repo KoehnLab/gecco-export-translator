fd45f2f38496de8d9b30dc69eeeabacc589515d0
Robert Adam <dev@robert-adam.de>
2023-12-04 15:59:01 +0100

    Implement AST transformation

--- bin/gecco_export_translator.py
+++ bin/gecco_export_translator.py
@@ -2,7 +2,7 @@
 
 from lark import Lark, Transformer
 
-from typing import List
+from typing import List, Tuple
 
 import os
 import argparse
@@ -47,20 +47,211 @@ class Index:
 @dataclass
 class Contraction:
     id: int
+    factor: float
     result: Tensor
     super_vertex_association: List[int]
     vertices: List[Tensor]
+    arcs: List[Arc]
+    external_arcs: List[Arc]
+    contraction_indices: List[Index]
+    external_indices: List[Index]
 
 
 class MyTransformer(Transformer):
-    def contraction(self, items):
-        print(items)
+    def start(self, contractions) -> List[Contraction]:
+        return list(contractions)
 
-    def DOUBLE(self, value):
+    def contraction(self, items) -> Contraction:
+        (
+            contr_id,
+            result_tensor,
+            factor,
+            num_vertices,
+            super_vertex_association,
+            num_arcs,
+            vertices,
+            arcs,
+            external_arcs,
+            contraction_indices,
+            external_indices,
+        ) = items
+
+        # Transform to 0-based indexing
+        assert type(contr_id) == int
+        assert contr_id > 0
+        contr_id -= 1
+
+        return Contraction(
+            id=contr_id,
+            factor=factor,
+            result=result_tensor,
+            super_vertex_association=super_vertex_association,
+            vertices=vertices,
+            arcs=arcs,
+            external_arcs=external_arcs,
+            contraction_indices=contraction_indices,
+            external_indices=external_indices,
+        )
+
+    def result_string(self, parts) -> List[Index]:
+        assert len(parts) % 5 == 0
+        nIndices = len(parts) // 5
+
+        result_indices: List[Index] = []
+
+        for i in range(nIndices):
+            vertex_idx = parts[i]
+            idx_type = parts[i + nIndices]
+            idx_space = parts[i + 2 * nIndices]
+            external = parts[i + 3 * nIndices]
+            # This data is duplicated
+            vertex_idx2 = parts[i + 4 * nIndices]
+            idx_id = parts[i + 5 * nIndices]
+
+            assert vertex_idx == vertex_idx2
+
+            # Transform to 0-based indexing
+            assert type(vertex_idx) == int
+            assert vertex_idx > 0
+            vertex_idx -= 1
+            assert type(idx_type) == int
+            assert idx_type > 0
+            idx_type -= 1
+            assert type(idx_space) == int
+            assert idx_space > 0
+            idx_space -= 1
+            assert type(idx_id) == int
+            assert idx_id > 0
+            idx_id -= 1
+
+            assert type(external) == str
+            assert external in ["T", "F"]
+            external = True if external == "T" else False
+
+            if external:
+                continue
+
+            idx = Index(id=idx_id, space=idx_space, vertex=vertex_idx, type=idx_type)
+            result_indices.append(idx)
+
+        assert not None in result_indices
+        return result_indices
+
+    def contr_string(self, parts) -> List[Index]:
+        assert len(parts) % 6 == 0
+        nIndices = len(parts) // 6
+
+        contraction_indices: List[Index] = []
+
+        for i in range(nIndices):
+            vertex_idx = parts[i]
+            idx_type = parts[i + nIndices]
+            idx_space = parts[i + 2 * nIndices]
+            external = parts[i + 3 * nIndices]
+            arc_idx = parts[i + 4 * nIndices]
+            idx_id = parts[i + 5 * nIndices]
+
+            # Transform to 0-based indexing
+            assert type(vertex_idx) == int
+            assert vertex_idx > 0
+            vertex_idx -= 1
+            assert type(idx_type) == int
+            assert idx_type > 0
+            idx_type -= 1
+            assert type(idx_space) == int
+            assert idx_space > 0
+            idx_space -= 1
+            assert type(arc_idx) == int
+            assert arc_idx > 0
+            arc_idx -= 1
+            assert type(idx_id) == int
+            assert idx_id > 0
+            idx_id -= 1
+
+            assert type(external) == str
+            assert external in ["T", "F"]
+            external = True if external == "T" else False
+
+            if external:
+                continue
+
+            idx = Index(id=idx_id, space=idx_space, vertex=vertex_idx, type=idx_type)
+            contraction_indices.append(idx)
+
+        assert not None in contraction_indices
+        return contraction_indices
+
+    def vertices(self, tensors) -> List[Tensor]:
+        return list(tensors)
+
+    def external_arcs(self, parts) -> List[Arc]:
+        return self.arcs(parts)
+
+    def arcs(self, parts) -> List[Arc]:
+        arcs = list(parts)
+
+        for current in arcs:
+            assert type(current) == Arc
+
+        return arcs
+
+    def arc_spec(self, components) -> Arc:
+        first_vertex_idx, second_vertex_idx, indexing = components
+        assert type(first_vertex_idx) == int
+        assert type(second_vertex_idx) == int
+
+        # Transform to 0-based indexing
+        assert first_vertex_idx > 0
+        assert second_vertex_idx > 0
+        first_vertex_idx -= 1
+        second_vertex_idx -= 1
+
+        return Arc(
+            first_vertex_idx=first_vertex_idx,
+            second_vertex_idx=second_vertex_idx,
+            indices=indexing,
+        )
+
+    def super_vertex(self, spec) -> List[int]:
+        vertex_association = list(spec)
+
+        # Transform to 0-based indexing
+        for i in range(len(vertex_association)):
+            assert type(vertex_association[i]) == int
+            assert vertex_association[i] > 0
+            vertex_association[i] -= 1
+
+        return vertex_association
+
+    def tensor_spec(self, components) -> Tensor:
+        name, transposed, indices = components
+        assert transposed in ["T", "F"]
+        transposed = True if transposed == "T" else False
+
+        return Tensor(name=name, indexing=indices, transposed=transposed)
+
+    def index_spec(self, spec) -> List[IndexGroup]:
+        assert len(spec) % 2 == 0
+        groups: List[IndexGroup] = []
+        for i in range(0, len(spec), 2):
+            creators = list(spec[i]) if spec[i] is not None else []
+            annihilators = list(spec[i + 1]) if spec[i + 1] is not None else []
+            groups.append(IndexGroup(creators=creators, annihilators=annihilators))
+
+        return groups
+
+    def factor(self, factors) -> float:
+        external, sign, contraction_factor = factors
+        return external * sign * contraction_factor
+
+    def DOUBLE(self, value) -> float:
         return float(value)
 
-    def INT(self, value):
+    def INT(self, value) -> int:
         return int(value)
+
+    def ID(self, characters) -> str:
+        return "".join(characters)
 
 
 def read_grammar() -> str:
@@ -96,7 +287,8 @@ def main():
         contents = export_file.read()
     tree = parser.parse(contents)
 
-    MyTransformer().transform(tree)
+    contractions: List[Contraction] = MyTransformer().transform(tree)
+    print(contractions)
 
 
 if __name__ == "__main__":
